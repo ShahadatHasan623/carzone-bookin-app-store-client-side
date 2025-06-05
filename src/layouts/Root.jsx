b2ad8104d7941939c079eb3af5d3a462ccf8b28e
shahadathasan743599@gmail.com <ShahadatHasan623>
2025-06-05 00:27:30 -0700

    added login page and register page and firebase authentication

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import Navbar from '../sheared/Navbar';
+import Footer from '../sheared/Footer';
 
 const Root = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
